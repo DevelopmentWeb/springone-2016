e8dc27163e17af94cab75ecb5859a23f238e1134
Thomas Risberg <trisberg@pivotal.io>
2016-07-29 11:00:38 -0400

    Updating spark-task demo

--- spark-task/src/main/java/com/springdeveloper/demo/SparkTaskApplication.java
+++ spark-task/src/main/java/com/springdeveloper/demo/SparkTaskApplication.java
@@ -2,15 +2,15 @@ package com.springdeveloper.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.test.IntegrationTest;
+import org.springframework.cloud.task.app.spark.yarn.SparkYarnTaskConfiguration;
+import org.springframework.cloud.task.sparkapp.common.SparkAppCommonTaskProperties;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
-@Import(org.springframework.cloud.task.app.spark.yarn.SparkYarnTaskConfiguration.class)
+@Import({SparkYarnTaskConfiguration.class, SparkAppCommonTaskProperties.class})
 public class SparkTaskApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Task");
 		SpringApplication.run(SparkTaskApplication.class, args);
 	}
 }
