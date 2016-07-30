adcd7b8b8b80f4d20653b962f27dda64902bca4c
Thomas Risberg <trisberg@pivotal.io>
2016-07-29 22:52:25 -0400

    Upgrading to spring-cloud-task-app-starters 1.0.1

--- spark-task/src/main/java/com/springdeveloper/demo/SparkTaskApplication.java
+++ spark-task/src/main/java/com/springdeveloper/demo/SparkTaskApplication.java
@@ -19,11 +19,10 @@ package com.springdeveloper.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.task.app.spark.yarn.SparkYarnTaskConfiguration;
-import org.springframework.cloud.task.sparkapp.common.SparkAppCommonTaskProperties;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
-@Import({SparkYarnTaskConfiguration.class, SparkAppCommonTaskProperties.class})
+@Import(SparkYarnTaskConfiguration.class)
 public class SparkTaskApplication {
 
 	public static void main(String[] args) {
